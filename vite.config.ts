8528044d9f71a9aa2ed9f4bf8100e4fe4c821d4f
toollabo-tamori <tamori@toollabo.jp>
2025-08-13 22:40:34 +0900

    plugin形式の修正

--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,27 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 import fs from 'fs';
 
 export default defineConfig({
+  plugins: [react()],
   build: {
-    target: 'esnext',
+    manifest: true,
     outDir: 'dist',
-    lib: {
-      entry: resolve(__dirname, 'client-entry.ts'),
-      formats: ['iife'],
-      name: 'GrowiPluginInputReplace',
-    },
     rollupOptions: {
-      output: {
-        entryFileNames: 'client.js',
-      },
+      input: ['/client-entry.tsx'],
     },
   },
   plugins: [
     {
-      name: 'generate-manifest',
+      name: 'generate-growi-manifest',
       writeBundle() {
         const manifest = {
           name: 'growi-plugin-input-replace',
           version: '0.0.1',
           description: 'A Growi plugin that replaces code placeholders with user inputs',
           type: 'script',
-          entry: 'client.js',
+          entry: 'client-entry.js',
           author: 'toollabo-tamori',
         };
         fs.writeFileSync(resolve(__dirname, 'dist/manifest.json'), JSON.stringify(manifest, null, 2));
